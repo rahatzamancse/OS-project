5712393e887bdcd493ad44dd36ee98dbf6a52229
azlathi <azlathi@gmail.com>
2022-03-30 11:24:56 -0700

    data is now sorted by uuid

--- processDataset.py
+++ processDataset.py
@@ -3,10 +3,30 @@ import json
 import os
 
 data = []
+users = {}
+unimportant_fields = ['timeZone', 'mobileCountryCode', 'batteryStatus']
 
+i = 0
 for file in os.listdir('datasets'):
+	i += 1
+	if i % 10 == 0:
+		print(i)
+	if i > 400:
+		break
 	for line in open('datasets/' + file):
-		data.append(json.loads(line))
+		event = json.loads(line)
+		event['apps'] = [app for app in event['apps'] if app['priority'] == 'Foreground app']
 
+		for field in unimportant_fields:
+			del event[field]
+	
+		uuid = event['uuid']
+		del event['uuid']
 
-print(len(data))
+		# if uuid in users.keys():
+		# 	users[uuid].append(event)
+		# else:
+		# 	users[uuid] = [event]
+		data.append(event)
+		
+print(len(users))
